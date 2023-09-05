410a0493d5b5e0f68fb74c0e111429aac5730ab9
Laura Mejía M <93622576+lauramejia900@users.noreply.github.com>
2023-09-04 20:56:50 -0500

    first commit

---
+++ js/scrip.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("boton");
+function enviar(event){
+  btn.style.backgroundColor = "pink";
+     alert("Estas consultando otro país para ver las noticias");
+    }
